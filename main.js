514bf50821327f05ceb7750e15ed0784c7e935f3
andrescorredorrincon <felipecorredor9010@gmail.com>
2021-03-29 20:29:29 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+
+
+
+// Codigo en funcion
+function Saludar(p1){
+    console.log(`Hola ${p1}`);
+    return null;
+}
+
+Saludar("Miguel Angel");
+Saludar("Marly");
+Saludar("Orlando");
+Saludar("felipe");
+
+
+// Codigo ens ecuencia
+let saludar;
+saludar = "Miguel Angel";
+console.warn(`Hola ${saludar}`);
+saludar = "Marly";
+console.warn(`Hola ${saludar}`);
+saludar = "Orlando";
+console.warn(`Hola ${saludar}`);
+saludar = "felipe";
+console.warn(`Hola ${saludar}`);
+
+
+
+
+
+
+
+
+
+
+// debugger;
+// let nombre;
+// let edad;
+// nombre = prompt("Ingrese su nombre");
+// edad = parseInt(prompt("Ingrese su edad"));
+// console.log(nombre);
+// console.log(typeof(nombre));
+// console.log(edad);
+// console.log(typeof(edad));
+
+
+
+
+
+
+
